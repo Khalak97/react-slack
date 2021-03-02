83354d57557db41053b7277f63cb683072612950
Khalak97 <valodyakh@gmail.com>
2021-03-02 14:27:36 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+//import Login from './components/Login';
 
 function App() {
-  return <div className="app">Slack</div>;
+  return (
+    <div className="app">
+      <Router>
+        <>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                <Chat />
+              </Route>
+            </Switch>
+          </AppBody>
+        </>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
