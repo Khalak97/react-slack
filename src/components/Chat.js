2f6d4df4d9fe4348c4a57fb241cdd58d8821d51f
Khalak97 <valodyakh@gmail.com>
2021-03-08 16:45:04 +0200

    chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,138 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+
+import { selectRoomId } from "../features/appSlice";
+
+import ChatInput from "./ChatInput";
+import Message from "./Message";
+
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 
 function Chat() {
-  return <div>Chat</div>;
+  const roomId = useSelector(selectRoomId);
+
+  const [roomDetails] = useDocument(
+    roomId && db.collection("rooms").doc(roomId)
+  );
+
+  const chatRef = useRef(null);
+
+  const [roomMessages, loading] = useCollection(
+    roomId &&
+      db
+        .collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
+
+  useEffect(() => {
+    chatRef?.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [roomId, loading]);
+
+  return (
+    <ChatContainer>
+      {roomDetails && roomMessages && (
+        <>
+          <Header>
+            <HeaderLeft>
+              <h4>
+                <strong>#{roomDetails?.data().name}</strong>
+                <StarBorderOutlinedIcon />
+              </h4>
+            </HeaderLeft>
+            <HeaderRight>
+              <p>
+                <InfoOutlinedIcon /> Details
+              </p>
+            </HeaderRight>
+          </Header>
+
+          <ChatMessages>
+            {roomMessages?.docs.map(doc => {
+              const { message, timestamp, user, userImage } = doc.data();
+
+              return (
+                <Message
+                  key={doc.id}
+                  message={message}
+                  timestamp={timestamp}
+                  user={user}
+                  userImage={userImage}
+                />
+              );
+            })}
+            <ChatBottom />
+          </ChatMessages>
+
+          <ChatInput
+            channelName={roomDetails?.data().name}
+            channelId={roomId}
+            chatRef={chatRef}
+          />
+        </>
+      )}
+    </ChatContainer>
+  );
 }
 
 export default Chat;
+
+const ChatContainer = styled.div`
+  flex: 0.7;
+  flex-grow: 1;
+  overflow-y: scroll;
+  margin-top: 60px;
+  /* Hide scrollbar for IE, Edge and Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+  > h4 {
+    display: flex;
+    text-transform: lowercase;
+  }
+  > h4 > .MuiSvgIcon-root {
+    margin-left: 10px;
+    font-size: 18px;
+  }
+`;
+
+const HeaderRight = styled.div`
+  > p {
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+  }
+  > p > .MuiSvgIcon-root {
+    margin-right: 8px;
+    font-size: 16px;
+  }
+`;
+
+const ChatMessages = styled.div``;
+
+const ChatBottom = styled.div`
+  padding-bottom: 200px;
+`;
