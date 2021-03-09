8a7294ea569f27cd4f09801e879404133e560d1e
Khalak97 <valodyakh@gmail.com>
2021-03-09 10:47:37 +0200

    login

--- src/App.js
+++ src/App.js
@@ -3,26 +3,35 @@ import styled from "styled-components";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
-//import Login from './components/Login';
+import Login from "./components/Login";
 
 function App() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className="app">
       <Router>
-        <>
-          <Header />
-          <AppBody>
-            <Sidebar />
-            <Switch>
-              <Route path="/" exact>
-                <Chat />
-              </Route>
-            </Switch>
-          </AppBody>
-        </>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <AppBody>
+              <Sidebar />
+              <Switch>
+                <Route path="/" exact>
+                  <Chat />
+                </Route>
+              </Switch>
+            </AppBody>
+          </>
+        )}
       </Router>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,63 @@
 import React from "react";
+import styled from "styled-components";
+
+import { auth, provider } from "../firebase";
+
+import { Button } from "@material-ui/core";
 
 function Login() {
-  return <div>Login</div>;
+  const signIn = e => {
+    e.preventDefault();
+
+    auth.signInWithPopup(provider).catch(error => alert(error.message));
+  };
+
+  return (
+    <LoginContainer>
+      <LoginInnerContainer>
+        <img
+          src="https://assets.brandfolder.com/pl546j-7le8zk-btwjnu/v/2925183/view@2x.png?v=1610642000/"
+          alt="logo"
+        />
+        <h1>Sign in</h1>
+        <p>Slack.com</p>
+
+        <Button onClick={signIn}>Sign in with Google</Button>
+      </LoginInnerContainer>
+    </LoginContainer>
+  );
 }
 
 export default Login;
+
+const LoginContainer = styled.div`
+  background-color: #f8f8f8;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+`;
+
+const LoginInnerContainer = styled.div`
+  padding: 100px;
+  text-align: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  > img {
+    object-fit: contain;
+    height: 100px;
+    margin-bottom: 40px;
+  }
+  > button {
+    margin-top: 50px;
+    text-transform: inherit;
+    background-color: #0a8d48;
+    color: white;
+    :hover {
+      background-color: #0a8d48;
+      color: white;
+      opacity: 0.8;
+      transition: all 0.2s;
+    }
+  }
+`;
